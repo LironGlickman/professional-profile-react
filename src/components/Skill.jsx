db87a2267d99903fd7d8eb7320fa07ab45cdbfba
Liron Glickman <7LIRONG7@GMAIL.COM>
2023-01-12 15:24:34 +0200

    remove warnings

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import programing from '../icons/programming.png'
-import blockchain from '../icons/blockchain.png'
+
 function Skill(props) {
     return <div className="skill-row" >
-        <img src={props.icon} className={`${props.align}`}></img>
+        <img src={props.icon} alt="skill-img" className={`${props.align}`}></img>
         <h3>{props.skill}</h3>
         <p>{props.description}</p>
     </div>
